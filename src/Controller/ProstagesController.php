6235d69a141226dba931bafe3b9a46526cab151a
Ainhoa Lenestour <alenestour@iutbayonne.univ-pau.fr>
2021-12-02 09:24:35 +0100

    indexation librairies

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -17,4 +17,34 @@ class ProstagesController extends AbstractController
             'controller_name' => 'ProstagesController',
         ]);
     }
+
+    /**
+     * @Route("/entreprises", name="prostages_entreprises")
+     */
+    public function afficherEntreprises(): Response
+    {
+        return $this->render('prostages/affichageEntreprises.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+
+    /**
+     * @Route("/formations", name="prostages_formations")
+     */
+    public function afficherFormations(): Response
+    {
+        return $this->render('prostages/affichageFormations.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+
+    /**
+     * @Route("/stages/{id}", name="prostages_stage")
+     */
+    public function afficherStages($id): Response
+    {
+        return $this->render('prostages/affichageStages.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
 }
