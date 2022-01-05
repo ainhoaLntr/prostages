32bd23627e22b60a6d66cbb1064449dd08e14409
Ainhoa Lenestour <alenestour@iutbayonne.univ-pau.fr>
2022-01-05 14:50:39 +0100

    Ajout d'un tuple dans la table Formation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,16 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Formation;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $dutInfo = new Formation();
+        $dutInfo->setNomCourt("DUT Info");
+        $dutInfo->setNomLong("DUT Informatique");
+        $manager->persist($dutInfo);
 
         $manager->flush();
     }
