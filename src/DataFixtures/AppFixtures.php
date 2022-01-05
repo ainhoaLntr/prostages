ecfea8e8588da5bae56d807436141f244f43fa2d
Ainhoa Lenestour <alenestour@iutbayonne.univ-pau.fr>
2022-01-05 18:25:27 +0100

    Création des entreprises, des formations et des stages

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,8 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Formation;
+use App\Entity\Entreprise;
+use App\Entity\Stage;
 
 class AppFixtures extends Fixture
 {
@@ -13,13 +15,65 @@ class AppFixtures extends Fixture
         // Création d'un générateur de données Faker
         $faker = \Faker\Factory::create('fr_FR');
 
-        $nbFormations = 15;
+        // Création des formations
+        $dutInfo = new Formation();
+        $dutInfo->setNomCourt("DUT Info");
+        $dutInfo->setNomLong("DUT Informatique");
+        $dutGim = new Formation();            
+        $dutGim->setNomCourt("DUT GIM");
+        $dutGim->setNomLong("DUT Génie Industriel et Maintenance");
+        $lpProg = new Formation();
+        $lpProg->setNomCourt("LP Prog");
+        $lpProg->setNomLong("Licence Pro Programmation Avancée");
+        $lpNum = new Formation();
+        $lpNum->setNomCourt("LP Num");
+        $lpNum->setNomLong("Licence Pro Métiers du Numérique");
+        $dutGea = new Formation();
+        $dutGea->setNomCourt("DUT GEA");
+        $dutGea->setNomLong("DUT Gestion des Entreprises et des Admin");
+        $dutTc = new Formation();
+        $dutTc->setNomCourt("DUT TC");
+        $dutTc->setNomLong("DUT Techniques de Commercialisation");
+        $lpBanque = new Formation();
+        $lpBanque->setNomCourt("LP Banque");
+        $lpBanque->setNomLong("Licence Pro Banque et Assurance");
+        $lpGs = new Formation();
+        $lpGs->setNomCourt("LP GS");
+        $lpGs->setNomLong("Licence Pro Gestion Salariale");
 
-        for ($i=1; $i < $nbFormations; $i++) { 
-            $dutInfo = new Formation();
-            $dutInfo->setNomCourt("DUT Info");
-            $dutInfo->setNomLong($faker->realText($maxNbChar = 40, $indexSize = 2));
-            $manager->persist($dutInfo);
+        $tableauFormations = array($dutInfo, $dutGim, $lpProg, $lpNum, $dutGea, $dutTc, $lpBanque, $lpGs);
+
+        foreach($tableauFormations as $tabFormations)
+        {
+            $manager->persist($tabFormations);
+        }
+
+        // Création des entreprises
+        $nbEntreprises = 12;
+
+        for ($i=0; $i < $nbEntreprises; $i++) { 
+            $entreprises = new Entreprise();
+            $entreprises->setNom($faker->compagny);
+            $entreprises->setAdresse($faker->address);
+            $entreprises->setActivite($faker->sentence);
+            $entreprises->setSitweb($faker->url);
+
+            $tabEntreprise[] = $entreprises;
+            $manager->persist($entreprises);
+        }
+
+        // Création des stages
+        $nbStages = 20;
+
+        for ($i=0; $i < $nbStages; $i++) { 
+            $stage = new Stage();
+            $stage->setTitre($faker->sentence);
+            $stage->setMission($faker->paragraph(4, false));
+            $stage->setEmail($faker->email);
+            $stage->setEntreprise();
+
+            
+            $manager->persist($entreprises);
         }
 
         // Envoi les données en BD
